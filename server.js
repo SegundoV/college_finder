2cca61bc35e442df707e570177cc3b642fc3b489
Jiheh Ritterling <jritterling@ts.com>
2018-08-01 20:20:27 -0400

    adding express

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const express = require('express')
 const app = express()
 
 app.use('/', express.static(path.join(__dirname + '/dist')));
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, () => console.log('App listening on port 5000'));
